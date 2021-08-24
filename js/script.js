c4b9a4ac5e6223398bdc1a44846ff6e133fb4b81
zharifzakaria <zharifzakaria@gmail.com>
2021-08-24 15:06:04 +0800

    js: fix 404

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let btn = document.getElementById("mobile-btn");
-
-btn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    let menu = document.getElementById("mobile-menu");
-    menu.classList.toggle("menu-show");
-    console.log("menu clicked");
-}
